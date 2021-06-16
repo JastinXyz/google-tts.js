af3ea6c8ec2793a09937e041789607217df517ef
JastinXyz <73673322+JastinXyz@users.noreply.github.com>
2021-06-16 12:14:11 +0700

    Update tts-file.js

--- src/tts-file.js
+++ src/tts-file.js
@@ -3,7 +3,7 @@ const userAgents = require("./tools/user-agents.js")
 const saveFile = async(text, lang, file) => {
   if(!text) throw new Error('Provide the text!')
   if(!lang) throw new Error('Provide language codes! e.g Indonesian = ID, English = EN. This is used for the sound type.')
-  if(lang.length > 2) throw new Error('Invalid Language codes (must 2 string length). All Language Codes: https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes')
+  if(lang.length >= 2) throw new Error('Invalid Language codes (must 2 string length). All Language Codes: https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes')
   
   const data = await axios(`https://translate.google.com/translate_tts?ie=UTF-8&q=${text.replace(/( )/ig, "%20").replace(/(\n)/ig, "%0A")}&tl=${lang}&total=1&idx=0&textlen=24&client=tw-ob&prev=input&ttsspeed=1`, {
       headers: {
