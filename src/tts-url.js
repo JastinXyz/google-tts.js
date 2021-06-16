cd7eefbfaecae8fd2317f586f7375eb0c90acd23
JastinXyz <73673322+JastinXyz@users.noreply.github.com>
2021-06-16 18:25:38 +0700

    Update tts-url.js

--- src/tts-url.js
+++ src/tts-url.js
@@ -1,7 +1,7 @@
-const saveUrl = async(text, lang) => {
+const getUrl = async(text, lang) => {
   if(!text) throw new Error('Provide the text!')
-  if(!lang) thow new Error('Provide language codes! e.g Indonesian = ID, English = EN. This is used for the sound type.')
-  if(lang.length >= 2) throw new Error('Invalid Language codes (must 2 string length). All Language Codes: https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes')
+  if(!lang) throw new Error('Provide language codes! e.g Indonesian = ID, English = EN. This is used for the sound type.')
+  if(lang.length > 2) throw new Error('Invalid Language codes (must 2 string length). All Language Codes: https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes')
   
   const data = `https://translate.google.com/translate_tts?ie=UTF-8&q=${text.replace(/( )/ig, "%20").replace(/(\n)/ig, "%0A")}&tl=${lang}&total=1&idx=0&textlen=24&client=tw-ob&prev=input&ttsspeed=1`
   const result = {
@@ -11,4 +11,4 @@ const saveUrl = async(text, lang) => {
   return result;
 }
 
-module.exports = saveUrl;
+module.exports = getUrl;
